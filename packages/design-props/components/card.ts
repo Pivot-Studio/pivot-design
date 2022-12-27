9a8208176418fc74ab118a35ecee0939700182ea
hoshinogami <3108764593@qq.com>
2022-12-27 20:42:57 +0800

    add:avatar

--- packages/design-props/components/card.ts
+++ packages/design-props/components/card.ts
@@ -6,92 +6,110 @@ export interface CardProps extends PivotDesignProps {
    * @description 自定义样式
    * @default undefined
    */
-  style?: React.CSSProperties&CardCssTokens
+  style?: React.CSSProperties & CardCssTokens;
   /**
    * @version 1.0.0
    * @description 自定义大小
    * @default undefined
    */
-  size?: 'small' | 'middle' | 'large'
+  size?: 'small' | 'middle' | 'large';
   /**
-  * @version 1.0.0
-  * @description 卡片内容是否加载
-  * @default middle
-  */
-  loading?: boolean
+   * @version 1.0.0
+   * @description 卡片内容是否加载
+   * @default middle
+   */
+  loading?: boolean;
   /**
-  * @version 1.0.0
-  * @description 卡片标题
-  * @default undefined
-  */
-  title?: string
+   * @version 1.0.0
+   * @description 卡片标题
+   * @default undefined
+   */
+  title?: string;
   /**
-  * @version 1.0.0
-  * @description 卡片底部的操作组
-  * @default undefined
-  */
-  actions?: Array<React.ReactNode>
+   * @version 1.0.0
+   * @description 卡片底部的操作组
+   * @default undefined
+   */
+  actions?: Array<React.ReactNode>;
   /**
-  * @version 1.0.0
-  * @description 是否有边框
-  * @default true
-  */
-  bordered?: boolean
+   * @version 1.0.0
+   * @description 是否有边框
+   * @default true
+   */
+  bordered?: boolean;
   /**
-  * @version 1.0.0
-  * @description 卡片右上角标签
-  * @default undefined
-  */
-  extra?: React.ReactNode
+   * @version 1.0.0
+   * @description 卡片右上角标签
+   * @default undefined
+   */
+  extra?: React.ReactNode;
   /**
    * @version 1.0.0
    * @description 时间标签
    * @default undefined
    */
-  time?: string
+  time?: string;
   /**
-  * @version 1.0.0
-  * @description 内容
-  * @default undefined
-  */
-  children?: React.ReactNode
-   /**
-  * @version 1.0.0
-  * @description 文字最大显示长度
-  * @default 4
-  */
- maxLength?: number
-  /**
-  * @version 1.0.0
-  * @description 展开按钮文字
-  * @default 展开
-  */
- expandText?: string
-  /**
-  * @version 1.0.0
-  * @description 收起按钮文字
-  * @default 收起
-  */
- collapseText?: string
- /**
-  * @version 1.0.0
-  * @description 封面
-  * @default undefined
-  */
-   cover?:React.ReactNode
-   /**
-  * @version 1.0.0
-  * @description 边框角
-  * @default true
-  */
- boradius?: boolean
-}
-interface CardCssTokens{
-  /**
-  * @version 1.0.0
-  * @description 卡片背景颜色
-  *@default --pivot-card-background-color
-  */
-   '--card-background-color'?:string;
+   * @version 1.0.0
+   * @description 内容
+   * @default undefined
+   */
+  children?: React.ReactNode;
 
+  /**
+   * @version 1.0.0
+   * @description 封面
+   * @default undefined
+   */
+  cover?: React.ReactNode;
+  /**
+   * @version 1.0.0
+   * @description 边框角
+   * @default true
+   */
+  boradius?: boolean;
+  /**
+   * @version 1.0.0
+   * @description 类型
+   * @default undefined
+   */
+  type?: string;
+  /**
+   * @version 1.0.0
+   * @description 头像
+   * @default true
+   */
+  avatar?: React.ReactNode;
+}
+interface CardCssTokens {
+  /**
+   * @version 1.0.0
+   * @description 卡片背景颜色
+   *@default --pivot-card-background-color
+   */
+  '--card-background-color'?: string;
+  /**
+   * @version 1.0.0
+   * @description 文字最大显示长度
+   * @default 6
+   */
+  '--card-maxLength'?: number;
+  /**
+   * @version 1.0.0
+   * @description 文字显示长度
+   * @default 3
+   */
+  '--card-minLength'?: number;
+  /**
+   * @version 1.0.0
+   * @description 展开按钮文字
+   * @default 展开
+   */
+  '--card-expandText'?: string;
+  /**
+   * @version 1.0.0
+   * @description 收起按钮文字
+   * @default 收起
+   */
+  '--card-collapseText'?: string;
 }
--- packages/design/components/Card/index.tsx
+++ packages/design/components/Card/index.tsx
@@ -25,6 +25,7 @@ const Card: React.FC<CardProps> = (props) => {
     extra,
     children,
     cover,
+    avatar,
     boradius = true,
   } = props;
   // const triggerClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -35,9 +36,7 @@ const Card: React.FC<CardProps> = (props) => {
   const [needExpandBtn, setNeedExpandBtn] = useState<boolean>(false);
   useEffect(() => {
     /** 组件加载完成，ref加载完成，初始状态判断。 */
-    setNeedExpandBtn(
-      descRef?.current?.scrollHeight > descRef?.current?.clientHeight
-    );
+    setNeedExpandBtn(descRef?.current?.scrollHeight > descRef?.current?.clientHeight);
   }, []);
 
   if (loading) {
@@ -53,14 +52,19 @@ const Card: React.FC<CardProps> = (props) => {
       [`${prefix}-card-borderadius`]: boradius,
     }
   );
-  const head = (title?: string, extra?: React.ReactNode, time?: string) => {
-    if (title || extra || time) {
+  const head = (title?: string, extra?: React.ReactNode, time?: string, avatar?: React.ReactNode) => {
+    if (title || extra || time || avatar) {
       return (
         <div className={`${prefix}-head`}>
           <div className={`${prefix}-head-wrapper`}>
-            {title && <div className={`${prefix}-head-title`}>{title}</div>}
+            {avatar && <div className={`${prefix}-head-avatar`}>{avatar}</div>}
+            {title && (
+              <div className={`${prefix}-head-title`}>
+                {title}
+                {time && <div className={`${prefix}-head-time`}>{time}</div>}
+              </div>
+            )}
             {extra && <div className={`${prefix}-head-extra`}>{extra}</div>}
-            {time && <div className={`${prefix}-head-time`}>{time}</div>}
           </div>
         </div>
       );
@@ -75,20 +79,13 @@ const Card: React.FC<CardProps> = (props) => {
     const btn = (children: React.ReactNode) => {
       return (
         <div className={`${prefix}-card-body-content`}>
-          <input
-            id={`${prefix}-card-body-btn-exp-${num}`}
-            className={`${prefix}-card-body-exp`}
-            type="checkbox"
-          />
+          <input id={`${prefix}-card-body-btn-exp-${num}`} className={`${prefix}-card-body-exp`} type="checkbox" />
 
           <div className={`${prefix}-card-body-text`} ref={descRef}>
             {children}
           </div>
           {needExpandBtn && (
-            <label
-              className={`${prefix}-card-body-btn`}
-              htmlFor={`${prefix}-card-body-btn-exp-${num}`}
-            ></label>
+            <label className={`${prefix}-card-body-btn`} htmlFor={`${prefix}-card-body-btn-exp-${num}`}></label>
           )}
         </div>
       );
@@ -96,9 +93,7 @@ const Card: React.FC<CardProps> = (props) => {
     // const text = (children: React.ReactNode) => {
     //   return <div className={`${prefix}-card-body-text`}>{children}</div>;
     // };
-    return (
-      <div className={classnames(`${prefix}-card-body`)}>{btn(children)}</div>
-    );
+    return <div className={classnames(`${prefix}-card-body`)}>{btn(children)}</div>;
   };
   const actionDom = (actions: React.ReactNode) => {
     return <div className={`${prefix}-actions`}>{actions}</div>;
@@ -107,7 +102,7 @@ const Card: React.FC<CardProps> = (props) => {
   return (
     <div className={classNames} style={style}>
       {cover}
-      {head(title, extra, time)}
+      {head(title, extra, time, avatar)}
       {body(children)}
       {actionDom(actions)}
     </div>
