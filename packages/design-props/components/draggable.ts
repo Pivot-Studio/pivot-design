d3eec21b1a2605a48e17b7ecfbb2bb0252669a4f
1360151219 <1360151219@qq.com>
2023-03-05 11:11:22 +0800

    feat: generate props

--- packages/design-props/components/draggable.ts
+++ packages/design-props/components/draggable.ts
@@ -27,3 +27,30 @@ export interface DraggableItemProps extends PivotDesignProps {
    */
   left?: number;
 }
+// test
+export interface DraggableProps extends PivotDesignProps {
+  /**
+   * @version 1.0.0
+   * @description 列表子项默认插槽
+   * @default undefined
+   */
+  children?: React.ReactNode;
+  /**
+   * @version 1.0.0
+   * @description 列表子项索引值
+   * @default undefined
+   */
+  index?: number;
+  /**
+   * @version 1.0.0
+   * @description 拖动元素相对定位的top偏移值
+   * @default undefined
+   */
+  top?: number;
+  /**
+   * @version 1.0.0
+   * @description 拖动元素相对定位的left偏移值
+   * @default undefined
+   */
+  left?: number;
+}
