d9c644cca302c238d92eec60a5944e7525668821
matto <1175526477@qq.com>
2023-05-10 11:34:31 +0800

    feat:add Input.Password

--- packages/design-props/index.ts
+++ packages/design-props/index.ts
@@ -1,7 +1,7 @@
-export type { ButtonProps } from './components/button';
+export * from './components/button';
 export type { DraggableItemProps } from './components/draggable';
 export type { IconProps } from './components/icon';
-export type { InputProps } from './components/input';
+export * from './components/input';
 export { CardProps } from './components/card';
 export { DraggableListProps, DraggableItemProps } from './components/draggableList';
 export { SkeletonProps } from './components/skeleton';
