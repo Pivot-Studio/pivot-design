2ace1bd4de54bbd3069cc83390f882eeb9aac266
1360151219 <zhanjingxi@ps.ink>
2023-08-13 23:44:19 +0800

    feat: switch组件支持尺寸、文本

--- packages/design/components/Switch/index.tsx
+++ packages/design/components/Switch/index.tsx
@@ -2,23 +2,35 @@ import classnames from 'classnames';
 import { prefix } from '../constants';
 import './index.scss';
 import useControlled from '../hooks/useControlled';
+import { ReactNode } from 'react';
 
 interface SwitchProps {
   value?: boolean;
   defaultValue?: boolean;
+  size?: 'normal' | 'large';
+  checkedText?: ReactNode;
+  uncheckedText?: ReactNode;
   onChange?: (e: boolean) => void;
 }
 
 function Switch(props: SwitchProps) {
   const [value, setValue] = useControlled(props);
+  const { size = 'normal', checkedText, uncheckedText } = props;
 
   return (
     <div
-      className={classnames(`${prefix}-switch`, {
+      className={classnames(`${prefix}-switch`, [`${prefix}-switch-size-${size}`], {
         checked: value,
       })}
     >
       <div className={classnames(`${prefix}-switch-pole`)} />
+      <div
+        className={classnames(`${prefix}-switch-text`, {
+          checked: value,
+        })}
+      >
+        {value ? checkedText : uncheckedText}
+      </div>
       <input
         type="checkbox"
         className={classnames(`${prefix}-switch-input`)}
